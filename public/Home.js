d97f4b4cf174416284b6aee4c69c2ae111596b77
Andi Achmad <emailanda@gmail.com>
2026-05-31 20:43:11 +0800

    second day

---
+++ public/Home.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Home</title>
+</head>
+<body>
+
+  <h1>Home Page</h1>
+
+  <p>Selamat datang di website Express pertama saya.</p>
+
+  <a href="/about">About</a>
+
+</body>
+</html>
